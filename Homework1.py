54cf29ac4935b7325b58031b7a3fec7088a0528e
Christopher <67124212+christorubio@users.noreply.github.com>
2020-09-16 10:14:51 -0500

    Update and rename Homework 1 to Homework1.py

--- Homework1.py
+++ Homework1.py
@@ -1,9 +1,9 @@
 #Chris Rubio
-#1530668  
+#1530668
 
 
 print('Birthday Calculator')
-print('Current day') 
+print('Current day')
 #current day variables
 month = int(input('Month: '))
 day = int(input('Day: '))
@@ -17,11 +17,11 @@ bday_year = int(input('Year: '))
 age = year - bday_year
 #if else to calculate age
 if month < bday_month:
-  age = age - 1 
-elif month == bday_month:
-  if day < bday_day:
     age = age - 1
-#if dates match 
+elif month == bday_month:
+    if day < bday_day:
+        age = age - 1
+#if dates match
 if month == bday_month and day == bday_day:
-  print('Happy Birthday!')
-print('You are', age,'years old.')
+    print('Happy Birthday!')
+print('You are', age, 'years old.')
