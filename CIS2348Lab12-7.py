91d2388b92721cb854f6c45d0140317ab7fe739c
Christopher <67124212+christorubio@users.noreply.github.com>
2020-11-23 18:27:56 -0600

    Create CIS2348Lab12-7.py

---
+++ CIS2348Lab12-7.py
@@ -0,0 +1,21 @@
+#Chris Rubio
+#1530668
+
+def get_age():
+    age = int(input())
+    if age < 18:
+        raise ValueError('Invalid age.')
+    if age > 75:
+        raise ValueError("Invalid age.")
+    return age
+
+def fat_burning_heart_rate(age):
+  return .70*(220 - age)
+   
+if __name__ == "__main__":
+    try:
+        age = get_age()
+        print("Fat burning heart rate for a", age, "year-old:", fat_burning_heart_rate(age), "bpm")
+    except ValueError as ve:
+        print(ve)
+        print("Could not calculate heart rate info.\n")
